1e751ae647e1660900e07dcc9d5a05354abd1b7f
Slava Filip <sl.filipps@gmail.com>
2020-04-11 18:15:06 +0300

    Class_04: inconsequential changes

--- src/class_03/Function.java
+++ src/class_03/Function.java
@@ -3,7 +3,6 @@ package class_03;
 public interface Function<Engineer, Result> {
 
     default Result apply(Engineer engineer) {
-        int anxiety;
         return null;
     }
 }
--- src/class_03/Runner.java
+++ src/class_03/Runner.java
@@ -1,25 +1,14 @@
 package class_03;
 
+import class_03.worker.AutomationEngineer;
+import class_03.worker.TestEngineer;
+
 public class Runner {
     public static void main(String[] args) {
         //Creating two instances of Engineers
-        ManualEngineer manualEngineer = new ManualEngineer();
+        TestEngineer manualEngineer = new TestEngineer();
         AutomationEngineer automationEngineer = new AutomationEngineer();
 
-        //Fot manual engineer
-        manualEngineer.setAge(30);manualEngineer.setName("Peter");manualEngineer.setSurName("Parker");
-        System.out.println(manualEngineer.getAge());
-        System.out.println(manualEngineer.getName()+" "+manualEngineer.getSurName());
-        manualEngineer.speak();
-        manualEngineer.inventCode();
-        manualEngineer.executeTest();
 
-        //Fot automation engineer
-        automationEngineer.setAge(40);automationEngineer.setName("Bruce");automationEngineer.setSurName("Banner");
-        System.out.println(automationEngineer.getAge());
-        System.out.println(automationEngineer.getName()+" "+automationEngineer.getSurName());
-        automationEngineer.speak();
-        automationEngineer.inventCode();
-        automationEngineer.executeTest();
     }
 }
