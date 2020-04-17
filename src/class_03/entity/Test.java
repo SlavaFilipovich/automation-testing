42ee7a546cd8cc99d868fd7a9672127daa3f00e3
Slava Filip <sl.filipps@gmail.com>
2020-04-18 01:23:14 +0300

    Class_04:some changes of Test and Engineer

--- src/class_03/entity/Test.java
+++ src/class_03/entity/Test.java
@@ -6,8 +6,8 @@ import class_03.worker.Engineer;
 import class_03.worker.TestEngineer;
 
 public abstract class Test implements Function<Engineer, Result> {
-    protected int complexity;
-    protected int instability;
+    private int complexity;
+    private int instability;
 
     public Test(TestLevel testLevel, int instability) {
         this.complexity = testLevel.COMPLEXITY;
@@ -25,12 +25,22 @@ public abstract class Test implements Function<Engineer, Result> {
         if ((engineer instanceof AutomationEngineer && this instanceof ManualTest) ||
                 (engineer instanceof TestEngineer && this instanceof AutomatedTest)){
             anxiety = engineer.getAnxiety();
-        } else
-            anxiety = 1;
+        } else{
+        engineer.setAnxiety(1);
+        anxiety = engineer.getAnxiety();
+        }
         int result = anxiety * instability * complexity/engineer.getSkill();
         if (result > 30)
             return Result.FAILED;
         else
             return Result.PASSED;
+    }
+
+    public int getComplexity() {
+        return complexity;
+    }
+
+    public int getInstability() {
+        return instability;
     }
 }
--- src/class_03/worker/Engineer.java
+++ src/class_03/worker/Engineer.java
@@ -5,18 +5,26 @@ import class_03.entity.Result;
 import class_03.entity.Test;
 
 public abstract class Engineer extends Person {
-    private int skill = (int) (Math.random() * 10 + 1);
+    private int skill = (int) (Math.random() * 10) + 1;
     private int anxiety = 3;
-
-    public int getAnxiety() {
-        return anxiety;
-    }
 
     public Result executeTest(Test test) {
         return this.executeTest(test);
     }
 
+    public int getAnxiety() {
+        return anxiety;
+    }
+
     public int getSkill() {
         return skill;
+    }
+
+    public void setSkill(int skill) {
+        this.skill = skill;
+    }
+
+    public void setAnxiety(int anxiety) {
+        this.anxiety = anxiety;
     }
 }
