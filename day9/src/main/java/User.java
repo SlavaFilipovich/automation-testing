dd68ae07b9cd32c83443590c1341ec68f1e59dc4
Slava Filip <sl.filipps@gmail.com>
2020-05-04 15:54:31 +0300

    DAY9: Java task to fill in the DB has completed

---
+++ day9/src/main/java/User.java
@@ -0,0 +1,41 @@
+public class User {
+    private int id;
+    private String login;
+    private String password;
+
+
+    User(int id, String login, String password) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("User with ID=%-2d [ login: %-10s, password: %-15s]",id,login,password);
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
