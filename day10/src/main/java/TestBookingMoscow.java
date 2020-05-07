b82f8d4ceb3a2c8557eae3e476c3e9f623c84b7f
Slava Filip <sl.filipps@gmail.com>
2020-05-07 21:03:16 +0300

    edit TestBookingParis, add TestBookingMoscow

--- day10/src/main/java/TestBookingMoscow.java
+++ day10/src/main/java/TestBookingMoscow.java
@@ -3,13 +3,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
-public class TestBookingParis {
+public class TestBookingMoscow {
     static WebDriver driver;
 
     public static void main(String[] args) throws InterruptedException {
@@ -17,16 +18,16 @@ public class TestBookingParis {
         System.setProperty("webdriver.chrome.driver", "C:\\Tools\\chromedriver.exe");
         driver = new ChromeDriver();
 
-        driver.navigate().to("https://booking.com");
+        driver.get("https://booking.com");
         WebElement elCity = driver.findElement(By.xpath("//*[contains(@placeholder,'Куда вы хотите поехать')]"));
-        elCity.sendKeys("Париж");
+        elCity.sendKeys("Москва");
         elCity.click();
 
         driver.findElement(By.xpath("//div[@data-calendar2-title='Приезжаю']")).click();
         Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.DAY_OF_YEAR, 3);
+        calendar.add(Calendar.DAY_OF_YEAR, 10);
         Date threeDays = calendar.getTime();
-        calendar.add(Calendar.DAY_OF_YEAR, 7);
+        calendar.add(Calendar.DAY_OF_YEAR, 5);
         Date tenDays = calendar.getTime();
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         String datePlusThreeDays = dateFormat.format(threeDays);
@@ -36,31 +37,31 @@ public class TestBookingParis {
         WebElement dateTo=driver.findElement(By.xpath(String.format("//*[contains(@data-date,'%s')]", datePlusTenDays)));
         dateTo.click();
 
-        WebElement settings = driver.findElement(By.xpath("//label[@class='xp__input']"));
-        settings.click();
-
+        Actions actions = new Actions(driver);
+        WebElement settingTrip = driver.findElement(By.xpath("//label[@class='xp__input']"));
         WebElement buttonAdults = driver.findElement(By.xpath("//*[contains(@aria-label, 'Взрослых: увеличить количество')]"));
-        buttonAdults.click();buttonAdults.click();
         WebElement buttonRoom = driver.findElement(By.xpath("//*[contains(@aria-label, 'Номера: увеличить количество')]"));
-        buttonRoom.click();
-        settings.click();settings.click();
         WebElement checkPrice = driver.findElement(By.xpath("//button[@class='sb-searchbox__button ']"));
-        checkPrice.click();
-        WebElement checkbox = driver.findElement(By.xpath("//a[@data-id='pri-5']"));
-        checkbox.click();
-        Thread.sleep(2000);
-        WebElement sortElement = driver.findElement(By.cssSelector("[class*=sort_price]"));
-        sortElement.click();
+        actions.click(settingTrip).moveToElement(buttonAdults).doubleClick()
+                .moveToElement(buttonRoom).click()
+                .moveToElement(settingTrip).doubleClick()
+                .moveToElement(checkPrice).click().build().perform();
 
-        String sampleMaxPrice = driver.findElement(By.xpath("//a[@data-id='pri-5']//span")).getText().replaceAll("[^0-9.]", "");
+        WebElement checkbox = driver.findElement(By.xpath("//a[@data-id='pri-1']"));
+        actions.click(checkbox).build().perform();
+
+        String sampleMaxPrice = driver.findElement(By.xpath("//a[@data-id='pri-1']//span"))
+                                        .getText()
+                                        .split("-")[1]
+                                        .replaceAll("[^0-9.]", "");
         int maxPrice = Integer.parseInt(sampleMaxPrice);
         //driver.manage().timeouts().pageLoadTimeout(3, TimeUnit.SECONDS);
         Thread.sleep(5000);
         List<WebElement> hotelsList = driver.findElements(By.xpath("//div[@class='bui-price-display__value prco-inline-block-maker-helper']"));
         String maxCost = hotelsList.get(1).getText().replaceAll("[^0-9.]", "");
-        int budgetNight = Integer.parseInt(maxCost) / 7;
+        int budgetNight = Integer.parseInt(maxCost) / 5;
 
-        if(budgetNight<maxPrice) {
+        if(budgetNight>maxPrice) {
             Assert.assertTrue("Something wrong", budgetNight>maxPrice);
         } else
             System.out.println(String.format("Budget per night: %s BYN\r\nMaxPrice: %s BYN",budgetNight,maxPrice));
